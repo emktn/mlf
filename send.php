8a6a97344524f04d05a45541da7a56bcf002c7fb
mktn <mktn@USERs-iMac.local>
2016-12-02 09:25:41 -0600

    Done

---
+++ send.php
@@ -0,0 +1,68 @@
+<?php
+
+define('SENDER_EMAIL', 'no-reply@website.com');
+define('RECEIVER_EMAIL', 'efrain@melimarketing.com');
+
+
+/*
+ * Common functions
+ */
+
+function sendMail($from, $to, $subject, $data) {
+
+    $headers = "MIME-Version: 1.0" . "\r\n";
+    $headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
+    $headers .= 'From: <'.$from.'>' . "\r\n";
+
+    return mail($to, $subject, $data, $headers);
+}
+
+function giveEmailerHTML($data=[]) {
+    $html = '<table cellpadding="2" cellspacing="5">';
+    foreach ($data as $record) {
+        $html .= '<tr>';
+        $html .= '<th>'.$record['name'].'</th>';
+        $html .= '<td>'.$record['value'].'</td>';
+        $html .= '<tr>';
+    }
+    $html .= '</table>';
+    return $html;
+}
+
+function input($str) {
+    return stripslashes(trim($str));
+}
+
+
+if ($_POST && isset($_POST['submit'])) {
+
+    $name = input($_POST['name']);
+    $email = input($_POST['email']);
+    $message = input($_POST['message']);
+
+    $subject = 'New request from your website!';
+
+    $data = array(
+        array(
+            'name' => 'Name',
+            'value' => $name
+        ),
+        array(
+            'name' => 'Email',
+            'value' => $email
+        ),
+        array(
+            'name' => 'Message',
+            'value' => $message
+        )
+    );
+
+    $mail1 = sendMail(SENDER_EMAIL, RECEIVER_EMAIL, $subject, giveEmailerHTML($data));
+
+    if($mail1) {
+      //  echo json_encode(['success' => true]);
+      header("Refresh:0; url=/");
+    } else {
+        echo json_encode(['success' => false]);
+    }
+}
